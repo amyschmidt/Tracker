250cca0e35016900af903a8c0e139a824f798f4b
Ryan Pliske <MIZIT@mwc-088183.dhcp.missouri.edu>
2014-12-11 13:31:13 -0600

    More Fixes For AirPlane Mode
    Essentially the errors that are occurring are occurring during testing.
    This update attempts to figure out if the app is in it’s initial
    session or if it’s not. It becomes not the initial session when the app
    delegate gets called again when you return to the app.

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var CloudDataObject:cloudData = cloudData()
     // Successfully launched app flag
     var appIsActive = false
+    var isInInitialSession = true
     // Widget Sharing Capabilities
     let sharedDefaults = NSUserDefaults(suiteName: "group.TrackerTeamA")
     
@@ -22,49 +23,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        println("Successfully Launched Tracker.")
         self.appIsActive = true
+        let activeString:String = self.appIsActive ? "Active":"Not Active"
+        println("Tracker Launched. App is \(activeString).")
+        self.isInInitialSession = true
         return true
     }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-        println("Tracker is about to be Inactive.")
         self.appIsActive = false
+        let activeString:String = self.appIsActive ? "Active":"Not Active"
+        println("Tracker is about to be \(activeString).")
+        self.isInInitialSession = true
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        self.appIsActive = false
         println("Tracker was sent to the Background..")
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        self.appIsActive = true
         println("Tracker Returned from the Background.")
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        println("Tracker Returned from being Inactive.")
-        // Reset the Count (Refresh TodayTabViewController)
-        // CloudDataObject.save_record_from_Widget()
-        
-        var rootViewController = self.window!.rootViewController
-        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        var setViewController = mainStoryboard.instantiateViewControllerWithIdentifier("TodayTab") as TodayTabViewController
-        // rootViewController.navigationController?.popToViewController(setViewController, animated: false)
+        self.appIsActive = true
+        let activeString:String = self.appIsActive ? "Active":"Not Active"
+        println("Tracker Became \(activeString).")
+
+        // If there is a date saved from the Widget
         if let date = self.sharedDefaults?.objectForKey("record") as? NSDate {
-            setViewController.receivedRecordFromWidget = true
-            // setViewController.count = 90
-            // setViewController.viewDidLoad()
-            // var count = 90
-            // setViewController.dailyCountLabel.text = "\(count)"
-            CloudDataObject.save_record_to_cloud(date)
-            CloudDataObject.grab_todays_records()
+
+            println("There is a record from the Widget. Attempting to Save it.")
+            if (!self.isInInitialSession)
+            {
+                CloudDataObject.save_record_to_cloud(date)
+                CloudDataObject.grab_todays_records()
+            }
         }
-        setViewController.updateCountFromWidget()
     }
 
     func applicationWillTerminate(application: UIApplication) {
